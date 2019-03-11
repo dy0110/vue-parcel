61b05aa5de7d982e1d0b39ab132c3f7ee7c71ee8
Daichi Yoshida <d.y.0110.barcelona@gmail.com>
2019-03-11 23:15:38 +0900

    ルーティング設定

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 export default {
     data() {
         return {
-            test: 'こんにちは'
+            test: 'トップページです'
         }
     },
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import MyApp from "./App";
+import router from "./router/router";
+import store from "./store";
 
 new Vue({
   el: "#app",
+  router,
+  store,
   render:h => h(MyApp)
 });
